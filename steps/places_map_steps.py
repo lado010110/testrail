3ca8befd799c55c2ca2b26fa276080d42b2712ea
xvtiskaci <ghvtisochkheidze@gmail.com>
2024-02-23 18:21:46 +0400

    jenkinsistvis v0.9

--- steps/places_map_steps.py
+++ steps/places_map_steps.py
@@ -1,14 +1,14 @@
-import time
 
 from pages.imereti_page import ImeretiPage
 from pages.main_page import MainPage
+from pages.map_page import MapPage
 from pages.places_page import PlacesPage
-from utils.DriverUtils import DriverUtils
 
 
 class PlacesMapSteps:
     @staticmethod
     def places_map():
+        map = MapPage()
         main_page = MainPage()
         main_page.click_places_button()
         places_page = PlacesPage()
@@ -17,13 +17,13 @@ class PlacesMapSteps:
         imereti_page.move_to_map()
         imereti_page.is_visible()
         imereti_page.screenshot_map()
-        imereti_page.click_more_toggle_places()
-        imereti_page.click_more_toggle_imereti()
-        imereti_page.click_sairme()
-        imereti_page.click_more_toggle_imereti()
-        imereti_page.click_more_toggle_places()
-        imereti_page.click_more_toggle_sights()
-        imereti_page.click_more_toggle_statue()
-        imereti_page.click_castle()
-        imereti_page.click_more_toggle_statue()
-        imereti_page.click_more_toggle_sights()
+        map.click_more_toggle_places()
+        map.click_more_toggle_imereti()
+        map.click_sairme()
+        map.click_more_toggle_imereti()
+        map.click_more_toggle_places()
+        map.click_more_toggle_sights()
+        map.click_more_toggle_statue()
+        map.click_castle()
+        map.click_more_toggle_statue()
+        map.click_more_toggle_sights()
